9e69006d02aa0536c403672bea6f0d3384698fec
Hamed Noroozi <noruzi206@gmail.com>
2024-03-24 18:59:21 +0330

    add Card in ProductsPage

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 
 import { useProducts } from '../context/ProductContext';
+import Card from '../components/Card';
 
 function ProductsPage() {
-   const products =useProducts();
-   console.log(products);
+   const products = useProducts();
+
    return (
-      <div>
-         products
+      <div className="flex justify-between">
+         <div className="w-full flex flex-wrap justify-between">
+            {!products.length && <p>Loading... </p>}
+            {
+               products.map(product => 
+               <Card key={product.id} data={product}/>)
+            }
+         </div>
+         <div>Sidebar</div>
       </div>
    );
 }
