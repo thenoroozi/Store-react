b70050e156251756137a06df30339b9ed7905221
Hamed Noroozi <noruzi206@gmail.com>
2024-03-25 17:37:00 +0330

    add search box

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,22 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { useProducts } from '../context/ProductContext';
 import Card from '../components/Card';
+import Loader from '../components/Loader';
 
 function ProductsPage() {
    const products = useProducts();
+   const [search, setSearch] = useState("")
 
+
+   const searchHandler = () => {
+      console.log(search);
+   }
    return (
-      <div className="flex justify-between">
-         <div className="w-full flex flex-wrap justify-between">
-            {!products.length && <p>Loading... </p>}
-            {
-               products.map(product => 
-               <Card key={product.id} data={product}/>)
-            }
+      <>
+         <div>
+            <input
+               type="text"
+               placeholder='Search...'
+               value={search}
+               onChange={e => setSearch(e.target.value.toLowerCase().trim())} />
+            <button onClick={searchHandler}>
+               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+               </svg>
+            </button>
          </div>
-         <div>Sidebar</div>
-      </div>
+
+         <div className='container flex'>
+            <div className="w-full flex flex-wrap justify-between">
+               {!products.length && <Loader />}
+               {
+                  products.map(product =>
+                     <Card key={product.id} data={product} />)
+               }
+            </div>
+            <div>sidebar</div>
+         </div>
+      </>
    );
 }
 
