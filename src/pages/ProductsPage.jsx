deffbfbe82483d452e1b15403687a16693d16759
Hamed Noroozi <noruzi206@gmail.com>
2024-03-25 17:38:31 +0330

    add category section

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -12,6 +12,13 @@ function ProductsPage() {
    const searchHandler = () => {
       console.log(search);
    }
+   const categoryHandler = (event) => {
+      const { tagName } = event.target;
+      if (tagName !== "LI") return;
+
+      const category = event.target.innerText.toLowerCase();
+      console.log(category);
+   }
    return (
       <>
          <div>
@@ -35,7 +42,21 @@ function ProductsPage() {
                      <Card key={product.id} data={product} />)
                }
             </div>
-            <div>sidebar</div>
+            <div>
+               <div>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                     <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0ZM3.75 12h.007v.008H3.75V12Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm-.375 5.25h.007v.008H3.75v-.008Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+                  </svg>
+                  <p>Categories</p>
+               </div>
+               <ul onClick={categoryHandler}>
+                  <li>All</li>
+                  <li>Electronics</li>
+                  <li>Jewelery</li>
+                  <li>Men's Clothing</li>
+                  <li>Women's Clothing</li>
+               </ul>
+            </div>
          </div>
       </>
    );
