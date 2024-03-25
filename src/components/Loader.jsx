f1b0c17277c07ddea84c22afbb1fc4ea26dd95c1
Hamed Noroozi <noruzi206@gmail.com>
2024-03-25 17:34:30 +0330

    add Loader component

---
+++ src/components/Loader.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Loader() {
+   return (
+      <div className='flex items-center'>
+         <div className="inline-block h-10 w-10 mr-3 animate-spin rounded-full border-4 border-solid border-primary border-e-transparent align-[-0.125em] text-danger motion-reduce:animate-[spin_1.5s_linear_infinite]"
+            role="status">
+         </div>
+         <span className='text-lg font-semibold'>Loading...</span>
+      </div>
+   );
+}
+
+export default Loader;
