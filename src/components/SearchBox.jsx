d642071c21e6bb320884ff09d145e6674f0bd40b
Hamed Noroozi <noruzi206@gmail.com>
2024-03-26 21:01:28 +0330

    add styles of searchBox

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -10,14 +10,18 @@ function SearchBox({ search, setSearch, setQuery }) {
    }
 
    return (
-      <div>
+      <div className='mx-2.5 mb-10 flex items-center justify-center md:justify-start'>
          <input
             type="text"
             placeholder='Search...'
             value={search}
-            onChange={e => setSearch(e.target.value.toLowerCase().trim())} />
-         <button onClick={searchHandler}>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            onChange={e => setSearch(e.target.value.toLowerCase().trim())}
+            className='mr-2.5 outline-none border-2 border-dashed border-primary p-2 w-60 text-[0.9rem] text-primary rounded-lg'
+          />
+         <button
+         className='p-1.5 bg-primary text-white border-none rounded-lg cursor-pointer' 
+         onClick={searchHandler}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-7 h-7">
                <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
             </svg>
          </button>
