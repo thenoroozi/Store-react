85eff84c792b84ad701546514d09054842bbec5c
Hamed Noroozi <noruzi206@gmail.com>
2024-03-27 20:12:12 +0330

    create CartContext for managing the count of product

---
+++ src/context/CartContext.jsx
@@ -0,0 +1,27 @@
+import React, { createContext, useContext, useReducer } from 'react';
+
+
+const initialState = {}
+
+const reducer = (state, action) => {
+   
+ }
+
+const CartContext = createContext();
+
+function CartProvider({ children }) {
+   const [state, dispatch] = useReducer(reducer, initialState)
+   return (
+      <CartContext.Provider value={{ state, dispatch }}>
+         {children}
+      </CartContext.Provider>
+   );
+}
+
+const useCart = () => {
+   const { state, dispatch } = useContext(CartContext)
+   return [state, dispatch]
+}
+
+export default CartProvider;
+export { useCart };
