4f5906e66c6e75a655698582a90421be2d192498
Hamed Noroozi <noruzi206@gmail.com>
2024-03-27 20:57:32 +0330

    add products to cart

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,11 +1,36 @@
 import React, { createContext, useContext, useReducer } from 'react';
+import { sumProducts } from '../helper/helper';
 
 
-const initialState = {}
+const initialState = {
+   selecetedItems: [],
+   itemsCounter: 0,
+   totalPrice: 0,
+   checkout: false
+}
 
 const reducer = (state, action) => {
-   
- }
+   switch (action.type) {
+      case "ADD_ITEM":
+         if (!state.selecetedItems.find(item => item.id === action.payload.id)) {
+            state.selecetedItems.push({ ...action.payload, quantity: 1 })
+         }
+         return {
+            ...state,
+            ...sumProducts(state.selecetedItems),
+            checkout:false,
+         }
+      case "ADD_ITEM":
+
+         break;
+      case "ADD_ITEM":
+
+         break;
+
+      default:
+         throw new Error("Invalid Action");
+   }
+}
 
 const CartContext = createContext();
 
