e23b0259eee93ba814371696e7f2987e406002d4
Hamed Noroozi <noruzi206@gmail.com>
2024-03-30 21:38:21 +0330

    complete the ckeckout page

---
+++ src/components/BasketSidebar.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+function BasketSidebar({ state, clickHandler }) {
+   return (
+      <div className='w-full p-4 border-2 border-primary  border-dashed rounded-2xl md:mx-4 md:w-60'>
+         <div className='flex items-center'>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-5 h-5 mr-1 text-primary">
+               <path strokeLinecap="round" strokeLinejoin="round" d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0 1 18 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3 1.5 1.5 3-3.75" />
+            </svg>
+            <p className='text-primary font-semibold mr-2'>Total:</p>
+            <span>{state.totalPrice} $</span>
+         </div>
+         <div className='flex items-center'>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-5 h-5 mr-1 text-primary">
+               <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 8.25h15m-16.5 7.5h15m-1.8-13.5-3.9 19.5m-2.1-19.5-3.9 19.5" />
+            </svg>
+            <p className='text-primary font-semibold mr-2'>Quantity: </p>
+            <span>{state.itemsCounter}</span>
+         </div>
+         <div className='flex items-center'>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-5 h-5 mr-1 text-primary">
+               <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z" />
+            </svg>
+            <p className='text-primary font-semibold mr-2'>Status: </p>
+            <span>{!state.ckeckout && "pending..."}</span>
+         </div>
+         <button onClick={() => clickHandler("CHECKOUT")}
+            className='mt-10 inline-block btn w-full text-base font-semibold'>Checkout</button>
+      </div>
+   );
+}
+
+export default BasketSidebar;
--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
 
-function CheckoutPage(props) {
+import { useCart } from '../context/CartContext';
+import { Link } from 'react-router-dom';
+
+import BasketCard from '../components/BasketCard';
+import BasketSidebar from '../components/BasketSidebar';
+
+function CheckoutPage() {
+   const [state, dispatch] = useCart();
+
+   const clickHandler = (type, data) => {
+      dispatch({ type, payload: data })
+   }
+
    return (
-      <div>
-         checkout page
+      <div className='container mx-auto px-4'>
+         <Link to="/products" className='btn w-fit px-2 ml-4 text-lg font-semibold items-center'>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={3} stroke="currentColor" className="w-5 h-5 mr-2">
+               <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
+            </svg>
+            Back to shop
+         </Link>
+         {state.itemsCounter>0 ?
+            <div className='my-8 flex flex-col-reverse items-center md:items-start md:flex-row'>
+               <BasketSidebar state={state}  clickHandler={clickHandler} />
+               <div className='w-full space-y-3 mb-3'>
+                  {state.selectedItems.map((product) => (
+                     <BasketCard key={product.id} data={product} clickHandler={clickHandler} />
+                  ))}
+               </div>
+            </div>
+            :
+            <div className='w-fit h-32 mx-auto my-36 px-5  text-center leading-[128px] border-2 border-primary border-dashed rounded-2xl text-xl font-semibold text-gray-500'>
+               Unfortunately there is no products!
+            </div>
+            }
       </div>
    );
 }
